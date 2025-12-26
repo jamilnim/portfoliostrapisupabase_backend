318200bb576c0d342431fed929db3e9840e094b0
jamilnim <jamilnimbook2@gmail.com>
2025-12-26 16:27:34 +0200

    dj correction

--- config/database.js
+++ config/database.js
@@ -3,7 +3,9 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     pool: { min: 2, max: 10 },
   },
