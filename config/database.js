4481f61699c684b87bc9a886dc7a90ed6c17d779
jamilnim <jamilnimbook2@gmail.com>
2025-12-26 13:56:51 +0200

    deployment ready

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: { min: 2, max: 10 },
+  },
+});
---
+++ config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = {
+    upload: {
+      config: {
+        provider: 'local',
+      },
+    },
+  };
+  
